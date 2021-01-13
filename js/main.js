28273a010e96df4b0fe8207d3b08a810f45278d6
Sargis <sargis.web@gmail.com>
2021-01-13 22:44:05 +0400

    home page is redy lasted only footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(function(){
-    console.log('Custom js is working fine.');
+    // Owl carousel
+    $(".owl-carousel").owlCarousel({
+        'loop': true,
+        responsive:{
+            0:{
+                items: 1,
+            },
+            576:{
+                items: 2,
+            },
+            992:{
+                items: 3
+            },
+            1300:{
+                items: 4
+            },
+            1600:{
+                items: 5
+            },
+        },
+    });
 });
