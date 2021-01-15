752d5d9678a0052b48e4fc90181150a8afa49e18
Sargis <sargis.web@gmail.com>
2021-01-15 12:22:13 +0400

    in details page lasted reviews section

--- js/main.js
+++ js/main.js
@@ -20,4 +20,18 @@ $(function(){
             },
         },
     });
+
+    // Tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl, {
+        delay: { "show": 100, "hide": 100 }
+      })
+    })
+
+    // Details image changing
+    $('.details-secondary-image').on('click', function(){
+        var imgSrc = $(this).attr('src');
+        $('.details-general-image').attr('src', imgSrc);
+    })
 });
